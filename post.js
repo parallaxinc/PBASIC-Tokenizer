ac7370e188703e5ad4126249f162eef38bd1b2a6
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-03-20 00:19:56 -0700

    fill out TModuleRec and tests

--- post.js
+++ post.js
@@ -8,6 +8,57 @@ function get32(buffer, starting){
   return buffer[starting] + (buffer[starting + 1] << 8) + (buffer[starting + 2] << 16) + (buffer[starting + 3] << 24);
 }
 
+function parse(resultBuffer){
+
+  var TModuleRec = {
+    Succeeded: resultBuffer[0],
+    //3 padding bytes
+    Error: Module.Pointer_stringify(get32(resultBuffer, 4)),
+    DebugFlag: resultBuffer[8],
+    TargetModule: resultBuffer[9],
+    //2 padding bytes
+    TargetStart: get32(resultBuffer, 12),
+    ProjectFiles: [
+      Module.Pointer_stringify(get32(resultBuffer, 16)),
+      Module.Pointer_stringify(get32(resultBuffer, 20)),
+      Module.Pointer_stringify(get32(resultBuffer, 24)),
+      Module.Pointer_stringify(get32(resultBuffer, 28)),
+      Module.Pointer_stringify(get32(resultBuffer, 32)),
+      Module.Pointer_stringify(get32(resultBuffer, 36)),
+      Module.Pointer_stringify(get32(resultBuffer, 40)),
+    ],
+    ProjectFilesStart: [
+      get32(resultBuffer, 44),
+      get32(resultBuffer, 48),
+      get32(resultBuffer, 52),
+      get32(resultBuffer, 56),
+      get32(resultBuffer, 60),
+      get32(resultBuffer, 64),
+      get32(resultBuffer, 68)
+    ],
+    Port: Module.Pointer_stringify(get32(resultBuffer, 72)),
+    PortStart: get32(resultBuffer, 76),
+    LanguageVersion: get32(resultBuffer, 80),
+    LanguageStart: get32(resultBuffer, 84),
+    SourceSize: get32(resultBuffer, 88),
+    ErrorStart: get32(resultBuffer, 92),
+    ErrorLength: get32(resultBuffer, 96),
+    EEPROM: resultBuffer.slice(100, 2148),
+    EEPROMFlags: resultBuffer.slice(2148, 4196),
+    VarCounts: {
+      bits: resultBuffer[4196],
+      nibbles: resultBuffer[4197],
+      bytes: resultBuffer[4198],
+      words: resultBuffer[4199]
+    },
+    PacketCount: resultBuffer[4200],
+    PacketBuffer: resultBuffer.slice(4201, 4201 + 2304)
+    //3 padding bytes
+  };
+
+  return TModuleRec;
+}
+
 function compile(myString){
 
   // Allocate space for string and extra '0' at the end
@@ -35,63 +86,44 @@ function compile(myString){
 
   var resultBuffer = new Buffer(result);
 
-  var count = resultBuffer[4200];
-  count = count <= 128 ? count : 128;
+  var TModuleRec = parse(resultBuffer);
 
-  var TModuleRec = {
-    Succeeded: resultBuffer[0],
-    //3 padding bytes
-    Error: Module.Pointer_stringify(get32(resultBuffer, 4)),
-    DebugFlag: resultBuffer[8],
-    TargetModule: resultBuffer[9],
-    //2 padding bytes
-    TargetStart: get32(resultBuffer, 12),
-    ProjectFiles: [
-      get32(resultBuffer, 16),
-      get32(resultBuffer, 20),
-      get32(resultBuffer, 24),
-      get32(resultBuffer, 28),
-      get32(resultBuffer, 32),
-      get32(resultBuffer, 36),
-      get32(resultBuffer, 40)
-    ],
-    ProjectFilesStart: [
-      get32(resultBuffer, 44),
-      get32(resultBuffer, 48),
-      get32(resultBuffer, 52),
-      get32(resultBuffer, 56),
-      get32(resultBuffer, 60),
-      get32(resultBuffer, 64),
-      get32(resultBuffer, 68)
-    ],
-    Port: get32(resultBuffer, 72),
-    PortStart: get32(resultBuffer, 76),
-    LanguageVersion: get32(resultBuffer, 80),
-    LanguageStart: get32(resultBuffer, 84),
-    SourceSize: get32(resultBuffer, 88),
-    ErrorStart: get32(resultBuffer, 92),
-    ErrorLength: get32(resultBuffer, 96),
-    EEPROM: resultBuffer.slice(100, 2148),
-    EEPROMFlags: resultBuffer.slice(2148, 4196),
-    VarCounts: {
-      bits: resultBuffer[4196],
-      nibbles: resultBuffer[4197],
-      bytes: resultBuffer[4198],
-      words: resultBuffer[4199]
-    },
-    PacketCount: count,
-    PacketBuffer: resultBuffer.slice(4201, 4201 + count * 18)
-    //3 padding bytes
-  };
-
-    // Free memory
+  // Free memory
   Module._free(dataHeap.byteOffset);
 
-  //todo, incorrect compile gives error?
   return TModuleRec;
 }
 
+function testRecAlignment(){
+
+  //sizeof struct in c was 6508
+  var data = new Uint8Array(6508);
+
+  // Get data byte size, allocate memory on Emscripten heap, and get pointer
+  var nDataBytes = data.length * data.BYTES_PER_ELEMENT;
+  var dataPtr = Module._malloc(nDataBytes);
+
+  // Copy data to Emscripten heap (directly accessed from Module.HEAPU8)
+  var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+  dataHeap.set(new Uint8Array(data.buffer));
+
+  Module.ccall('TestRecAlignment', 'number', ['number'], [dataHeap.byteOffset]);
+
+  var result = new Uint8Array(dataHeap.buffer, dataHeap.byteOffset, data.length);
+
+  var resultBuffer = new Buffer(result);
+
+  var TModuleRec = parse(resultBuffer);
+
+  // Free memory
+  Module._free(dataHeap.byteOffset);
+
+  return TModuleRec;
+}
+
+
 module.exports = {
   version: version,
-  compile: compile
+  compile: compile,
+  testRecAlignment: testRecAlignment
 };
---
+++ test/tokenizer.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var expect = require('expect');
+
+var bs2tokenize = require('../');
+
+
+describe('tokenizer', function(){
+
+  it('#returns version 130', function(done){
+
+    var version = bs2tokenize.version();
+    expect(version).toEqual(130);
+    done();
+  });
+
+  it('#is properly aligned', function(done){
+
+    var EEPROM = new Buffer(2048);
+    EEPROM.fill(25);
+
+    var EEPROMFlags = new Buffer(2048);
+    EEPROMFlags.fill(26);
+
+    var PacketBuffer = new Buffer(2286);
+    PacketBuffer.fill(32);
+    PacketBuffer = Buffer.concat([PacketBuffer, new Buffer([0x34, 0x00, 0x35, 0x00, 0x36, 0x00, 0x37, 0x00, 0x38, 0x00, 0x39, 0x00, 0x31, 0x30, 0x00, 0x31, 0x38, 0x00])]);
+
+    var TModuleRec = bs2tokenize.testRecAlignment();
+    expect(TModuleRec.Succeeded).toEqual(0);
+    expect(TModuleRec.Error).toEqual('');
+    expect(TModuleRec.DebugFlag).toEqual(1);
+    expect(TModuleRec.TargetModule).toEqual(2);
+    expect(TModuleRec.TargetStart).toEqual(3);
+    expect(TModuleRec.ProjectFiles).toEqual(['4', '5', '6', '7', '8', '9', '10']);
+    expect(TModuleRec.ProjectFilesStart).toEqual([11, 12, 13, 14, 15, 16, 17]);
+    expect(TModuleRec.Port).toEqual('18');
+    expect(TModuleRec.PortStart).toEqual(19);
+    expect(TModuleRec.LanguageVersion).toEqual(20);
+    expect(TModuleRec.LanguageStart).toEqual(21);
+    expect(TModuleRec.SourceSize).toEqual(22);
+    expect(TModuleRec.ErrorStart).toEqual(23);
+    expect(TModuleRec.ErrorLength).toEqual(24);
+    expect(TModuleRec.VarCounts).toEqual({bits: 27, nibbles: 28, bytes: 29, words: 30});
+    expect(TModuleRec.EEPROM).toEqual(EEPROM);
+    expect(TModuleRec.EEPROMFlags).toEqual(EEPROMFlags);
+    expect(TModuleRec.PacketCount).toEqual(31);
+    expect(TModuleRec.PacketBuffer).toEqual(PacketBuffer);
+
+    done();
+  });
+
+  it('#successfuly compiles', function(done){
+
+    var myString = '\'{$STAMP BS2}\n' +
+    'Counter VAR BYTE\n' +
+    'FOR Counter = 1 to 20\n' +
+    '  PULSOUT 0,50000\n' +
+    '  PAUSE 250\n' +
+    'NEXT\n' +
+    'STOP';
+
+    var empty = new Buffer(2000);
+    empty.fill(0);
+
+    var empty2 = new Buffer(2268);
+    empty2.fill(0);
+
+    var EEPROM = new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x14, 0x20, 0x8C, 0x0E, 0xD8, 0xC8, 0x0E, 0x60, 0x4A, 0xAE, 0xE8, 0x9F, 0x49, 0xC1, 0x50, 0xC3, 0x6F, 0x8D, 0xD1, 0x03, 0x07, 0xC0]);
+    EEPROM = Buffer.concat([empty, EEPROM]);
+
+    var EEPROMFlags = new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83, 0x83]);
+    EEPROMFlags = Buffer.concat([empty, EEPROMFlags]);
+
+    var PacketBuffer = new Buffer([0xFE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x14, 0x20, 0x8C, 0x0E, 0xD8, 0xC8, 0x7C, 0xFF, 0x0E, 0x60, 0x4A, 0xAE, 0xE8, 0x9F, 0x49, 0xC1, 0x50, 0xC3, 0x6F, 0x8D, 0xD1, 0x03, 0x07, 0xC0, 0x60]);
+    PacketBuffer = Buffer.concat([PacketBuffer, empty2]);
+
+    var TModuleRec = bs2tokenize.compile(myString);
+
+    expect(TModuleRec.Succeeded).toEqual(1);
+    expect(TModuleRec.Error).toEqual('');
+    expect(TModuleRec.DebugFlag).toEqual(0);
+    expect(TModuleRec.TargetModule).toEqual(2);
+    expect(TModuleRec.TargetStart).toEqual(9);
+    expect(TModuleRec.LanguageVersion).toEqual(200);
+    expect(TModuleRec.LanguageStart).toEqual(0);
+    expect(TModuleRec.VarCounts).toEqual({bits: 0, nibbles: 0, bytes: 1, words: 0});
+    expect(TModuleRec.EEPROM).toEqual(EEPROM);
+    expect(TModuleRec.EEPROMFlags).toEqual(EEPROMFlags);
+    expect(TModuleRec.PacketCount).toEqual(2);
+    expect(TModuleRec.PacketBuffer).toEqual(PacketBuffer);
+
+    done();
+  });
+
+it('#returns an error string', function(done){
+
+  var myString = '\'{$STAMP BS2}\n' +
+  ' VAR BYTE\n' + //should be  'Counter VAR BYTE\n' +
+  'FOR Counter = 1 to 20\n' +
+  '  PULSOUT 0,50000\n' +
+  '  PAUSE 250\n' +
+  'NEXT\n' +
+  'STOP';
+
+  var TModuleRec = bs2tokenize.compile(myString);
+
+  expect(TModuleRec.Succeeded).toEqual(0);
+  expect(TModuleRec.Error).toEqual('149-Expected a label, variable, or instruction');
+  expect(TModuleRec.DebugFlag).toEqual(0);
+  expect(TModuleRec.TargetModule).toEqual(2);
+  expect(TModuleRec.TargetStart).toEqual(9);
+  expect(TModuleRec.LanguageVersion).toEqual(200);
+  expect(TModuleRec.LanguageStart).toEqual(0);
+  done();
+});
+
+});
